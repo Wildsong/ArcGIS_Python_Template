bcfb4f596aea9c1f9e451389d6e1be8405b574a7
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-06-21 13:35:27 -0700

    updates for Pro

--- field_update_tool.py
+++ field_update_tool.py
@@ -20,7 +20,7 @@ class Field_Update_tool(object):
     """This class has the methods you need to define
        to use your code as an ArcGIS Python Tool."""
         
-    def __init__(self):
+    def __init__(self) -> None:
         """Define the tool (tool name is the name of the class)."""
         self.label = "Field Update tool"
         self.description = """Update a field in a feature class."""
@@ -28,7 +28,7 @@ class Field_Update_tool(object):
         self.category = "Example" # Use your own category here, or an existing one.
         #self.stylesheet = "" # I don't know how to use this yet.
         
-    def getParameterInfo(self):
+    def getParameterInfo(self) -> list:
         """Define parameter definitions.
 Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python/defining-parameters-in-a-python-toolbox.htm
         """       
@@ -54,7 +54,7 @@ Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python
             parameterType="Required", # Required|Optional|Derived
             direction="Input", # Input|Output
         )
-        # Define this so that the list of field names will be filled in in ArcCatalog
+        # Define this so that the list of field names will be filled in automatically.
         field.parameterDependencies = [input_fc.name]
 
         # params[2] = a date/time thing
@@ -69,7 +69,7 @@ Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python
         
         # params[3] = a number that has to be in a range
         fixedrange = [100,500]
-        number = arcpy.Parameter(name="another_number",
+        number = arcpy.Parameter(name="a_number",
             displayName="A number in the range %s-%s" % (fixedrange[0],fixedrange[1]),
             datatype="GPLong",
             parameterType="Required", # Required|Optional|Derived
@@ -81,7 +81,16 @@ Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python
         # You can set a default value here.
         number.value = 200
         
-        # params[4] 
+        # params[4] = a number that has to be in a range
+        depnumber = arcpy.Parameter(name="dependent_number",
+            displayName="a number that's set by the previous field",
+            datatype="GPLong",
+            parameterType="Required", # Required|Optional|Derived
+            direction="Input", # Input|Output
+        )
+        depnumber.value = number.value * 10
+        
+        # params[5] = the output 
         output_fc = arcpy.Parameter(name="output_fc",
             displayName="Output feature class",
             datatype="DEFeatureClass",
@@ -95,36 +104,49 @@ Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python
         # See http://desktop.arcgis.com/en/desktop/latest/analyze/creating-tools/updating-schema-in-a-python-toolbox.htm#ESRI_SECTION1_0F3D82FC6ACA421E97AC6D23D95AF19D
         output_fc.schema.clone = True
 
-        return [input_fc, field, datestamp, number, output_fc]
+        return [input_fc, field, datestamp, number, depnumber, output_fc]
 
-    def isLicensed(self):
+    def isLicensed(self) -> bool:
         """Set whether tool is licensed to execute."""
         return True
 
-    def updateParameters(self, parameters):
+    def updateParameters(self, parameters) -> None:
         """Modify the values and properties of parameters before internal
         validation is performed.  This method is called whenever a parameter
         has been changed."""
-
         if parameters[0].altered:
-            # If the field is not in the new feature class
-            # then switch to the first field
-            try:
-                l = [f.name for f in arcpy.ListFields(parameters[0].valueAsText)]
-                if not parameters[1].valueAsText in l:
-                    parameters[1].value = l[0]
-            except:
-                # Could not read the field list
-                parameters[1].value = ""
+            if not arcpy.Exists(parameters[0].value):
+                parameters[0].setErrorMessage("Feature class could not be opened.")
+            else:
+                try:
+                    # Get a new list of fields. (Note the docs say not to use "ListFields" method.)
+                    l = [f.name for f in arcpy.da.Describe(parameters[0].valueAsText)['fields']]
+                    # If the field is not in the list, default to the first field found.
+                    if not parameters[1].valueAsText in l:
+                        parameters[1].value = l[0]
+                except:
+                    parameters[1].value = "" # Clear out the old field name
+                    parameters[1].setWarningMessage("Could not read the field list.")
 
-        if not parameters[3].altered:
-            # When you change the field called "number" then this function will
-            # be called and the next field will change to its value * 100.        
-            parameters[3].value = int(parameters[2].value)*100
+        if parameters[1].altered:
+            # It might be better to only show a list of date fields but this is a demo...
+            found = False
+            for f in arcpy.da.Describe(parameters[0].valueAsText)['fields']:
+                if parameters[1].valueAsText == f.name:
+                    if f.type != 'Date':
+                        parameters[1].setErrorMessage("Selected field has to be a date.")
+                    found = True
+            if not found:
+                parameters[1].setErrorMessage("No such field [%s]" % parameters[1].valueAsText)
+
+        if parameters[3].altered:
+            # When you change the field called "a_number" then this method will
+            # be called and the next field will change to its value * 10.        
+            parameters[4].value = int(parameters[3].value)*10
 
         return
 
-    def updateMessages(self, parameters):
+    def updateMessages(self, parameters) -> None:
         """Modify the messages created by internal validation for each tool
         parameter.  This method is called after internal validation."""
 
@@ -132,13 +154,13 @@ Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python
             parameters[2].setWarningMessage("Sample warning, you set the number to 2.")
         return
 
-    def execute(self, parameters, messages):
-        """The source code of your tool."""
+    def execute(self, parameters, messages) -> None:
+        """This is the code that executes when you click the "Run" button."""
         
         # Let's dump out what we know here.
         messages.addMessage("This is a test of your tool.")
         for param in parameters:
-            messages.addMessage("Parameter: %s = %s" % (param.name, param.valueAsText) )
+            self.describeParameter(messages,param)
         
         # Get the parameters from our parameters list,
         # then call a generic python function.
@@ -150,14 +172,26 @@ Refer to https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python
         input_fc  = parameters[0].valueAsText
         fieldname = parameters[1].valueAsText
         datestamp = parameters[2].valueAsText
-        depnumber = parameters[3].value
-        output_fc = parameters[4].valueAsText
+        number    = parameters[3].value
+        depnumber = parameters[4].value
+        output_fc = parameters[5].valueAsText
         
-        set_field_value(input_fc, fieldname, datestamp)
         # Okay finally go ahead and do the work.
-        messages.addMessage("Okay, I put \"%s\" in the \"%s\" field in \"%s\"." % 
-            (datestamp, fieldname, output_fc))
+        try:
+            set_field_value(input_fc, fieldname, datestamp)
+            messages.addMessage("Okay, I put \"%s\" in the \"%s\" field in \"%s\"." % 
+                (datestamp, fieldname, output_fc))
+        except Exception as e:
+            arcpy.AddError("Fail. %s" % e)
         return
+
+    def describeParameter(self, m, p):
+        m.addMessage("===Parameter=== %s \"%s\"" % (p.name, p.displayName))
+        m.addMessage("  altered? %s" % p.altered)
+        m.addMessage("  value \"%s\"" % p.valueAsText)
+        m.addMessage("  datatype %s" % p.datatype)
+        m.addMessage("  filter %s" % p.filter)
+
     
 # =============================================================================
 if __name__ == "__main__":
@@ -180,7 +214,7 @@ if __name__ == "__main__":
     params[1].value = "datestamp"
     params[2].value = "2021/07/08 12:34"
     params[3].value = 100
-    params[4].value = "testing_output"
+    params[5].value = "testing_output"
     
     # Run it.
     update_datestamp.execute(params, Messenger())
